68ff2c33341c6c970c23372e6981d319a583d4fa
Code Ass <aycabta@gmail.com>
2014-03-27 07:17:53 +0900

    replace IO.readlines.each with IO.each

--- lambda_cat.rb
+++ lambda_cat.rb
@@ -2,7 +2,7 @@ puts 'begin'
 puts '  [(\\'
 ARGV.each do |filename|
   open(filename, 'r') do |f|
-    f.readlines.each do |line|
+    f.each do |line|
       puts "    lambda { |l| lambda { puts '#{line.chomp.gsub(/'|\\/, "'" => "\\'", '\\' => '\\\\')}'; l } }.call\\"
     end
   end
