f5aca244a66dba0595cd4a14e5e24d943aabb9b5
Code Ass <aycabta@gmail.com>
2014-03-27 00:53:42 +0900

    fix escape logic

--- lambda_printer.rb
+++ lambda_printer.rb
@@ -2,7 +2,7 @@ lines_sum = 0
 ARGV.each do |filename|
   open(filename, 'r') do |f|
     f.readlines.reverse.each do |line|
-      puts "lambda{ |l| puts '#{line.chomp.gsub("'", "\\'")}'; l.() }.call\\"
+      puts "lambda{ |l| puts '#{line.chomp.gsub(/'|\\/, "'" => "\\'", '\\' => '\\\\')}'; l.() }.call\\"
       lines_sum += 1
     end
   end
