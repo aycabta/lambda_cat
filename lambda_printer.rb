bfb7194c089b75889f03f48c3ee30848f9fdc162
Code Ass <aycabta@gmail.com>
2014-03-27 00:41:25 +0900

    fix output sequence of lines

--- lambda_printer.rb
+++ lambda_printer.rb
@@ -1,7 +1,7 @@
 lines_sum = 0
 ARGV.each do |filename|
   open(filename, 'r') do |f|
-    f.readlines.each do |line|
+    f.readlines.reverse.each do |line|
       puts "lambda{ |l| puts '#{line.chomp.gsub("'", "\\'")}'; l.() }.call\\"
       lines_sum += 1
     end
