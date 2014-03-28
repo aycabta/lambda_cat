6b1eb8b6fcc1fad1d24881ec60ffad246d8630ff
Code Ass <aycabta@gmail.com>
2014-03-28 13:36:35 +0900

    test with Travis CI

--- test.sh
+++ test.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-ruby lambda_cat.rb test | ruby | ( cat test | diff /dev/fd/3 -) 3<&0
+ruby lambda_cat.rb test | ruby | ( cat test | diff -u /dev/fd/3 -) 3<&0
+
+if test $? -eq 0; then
+	echo 'success'
+	return 0
+else
+	echo 'failure'
+	return 1
+fi
 
